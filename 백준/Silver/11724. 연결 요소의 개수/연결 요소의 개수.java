1a7bc51ce091c054b7594f3d3f4152ab35ca3339
SangHyeon EUN <91714677+COBI-98@users.noreply.github.com>
2023-01-19 16:21:01 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 552 ms, Memory: 116788 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,53 +1,61 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int[][] graph = new int[1001][1001];
-	static int V;
-	static int E;
-	static boolean[] visited = new boolean[1001];
-	
-	public static void dfs(int index) {
-		if(visited[index] == true)
-			return;
-		else {
-			visited[index] = true;
-			for(int i = 1; i <= V; i++) {
-				if(graph[index][i] == 1) {
-					dfs(i);
-				}
-			}
-		}
-	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in); 
-		
-		V = sc.nextInt();
-		E = sc.nextInt();
-		
-		int a,b;
-		for(int i = 0; i < E; i++) {
-			a = sc.nextInt();
-			b = sc.nextInt();
-			
-			// 간선 연결
-			graph[a][b] = graph[b][a] = 1;
-		}
-		
-		int result = 0 ;
-		
-		// dfs 탐색
-		for(int i = 1; i <= V; i++) {
-			if(visited[i] == false) { // 방문한 적 없는 노드라면 dfs.
-				dfs(i);
-				result++;
-			}
-		}
-		
-		System.out.println(result);
-		
-		sc.close();
-		return;
-	}
+    public static int vertex;
+    public static int trunk;
+    public static int [][] graph;
+    public static boolean [] visit;
+    public static int count;
+
+    public static Queue<Integer> queue = new LinkedList<>();
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        vertex = Integer.parseInt(st.nextToken());
+        trunk = Integer.parseInt(st.nextToken());
+
+        graph = new int[vertex+1][vertex+1];
+        visit = new boolean[vertex+1];
+        for (int i = 0; i < trunk; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            graph[A][B] = graph[B][A] = 1;
+        }
+
+        count = 0 ;
+
+        for (int i = 1; i <= vertex; i++) {
+            if (!visit[i]){
+                bfs(i);
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    public static void bfs(int node){
+        visit[node] = true;
+        queue.offer(node);
+
+        while (!queue.isEmpty()){
+            int cur = queue.poll();
+            for (int i = 1; i <= vertex; i++) {
+                if(!visit[i] && graph[cur][i] == 1) {
+                    visit[i] = true;
+                    queue.offer(i);
+                }
+            }
+        }
+
+    }
 }
