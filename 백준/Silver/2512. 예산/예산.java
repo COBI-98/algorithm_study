2a3c900ccb069ee44e5175f3c74a5887f0e3cf6e
SangHyeon EUN <91714677+COBI-98@users.noreply.github.com>
2023-01-26 17:22:11 +0900

    [Silver III] Title: 예산, Time: 184 ms, Memory: 15844 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int N;
+    public static int[] arr;
+
+    public static int totalRequest;
+    public static int totalMoney;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            totalRequest = totalRequest + arr[i];
+        }
+        totalMoney = Integer.parseInt(br.readLine());
+
+        Arrays.sort(arr);
+
+        System.out.println(binarySearch());
+    }
+
+    public static int binarySearch(){
+
+        if (totalRequest <= totalMoney){
+            return arr[arr.length-1];
+        }
+
+        int left = 0;
+        int right  = totalMoney;
+
+        while(left <= right){
+            int sum = 0;
+            int mid = (left + right) /2;
+
+            for (int i = 0; i < N; i++) {
+                if(arr[i] > mid){
+                    sum += mid;
+                }else{
+                    sum += arr[i];
+                }
+            }
+
+            if (sum > totalMoney){
+                right = mid -1;
+            }else if (sum < totalMoney){
+                left = mid+1;
+            }else{
+                return mid;
+            }
+        }
+
+        return right;
+
+    }
+}
