ff8c51586373d7fdbb983980dd0732e393b72f96
SangHyeon EUN <91714677+COBI-98@users.noreply.github.com>
2023-08-14 16:18:57 +0900

    [Silver II] Title: 병사 배치하기, Time: 164 ms, Memory: 14752 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[] arr;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        System.out.println(N - solve());
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        arr = new int[N];
+        dp = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    private static int solve() {
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            result = Math.max(result, dp[i]);
+        }
+        return result;
+    }
+    
+}
