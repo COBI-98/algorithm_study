ff979140971f8386c774120844f0c3dc25b00e05
SangHyeon EUN <91714677+COBI-98@users.noreply.github.com>
2023-09-18 23:39:59 +0900

    [Gold IV] Title: 키 순서, Time: 744 ms, Memory: 36380 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+    static int[][] dist;
+    static final int INF = 987654321;
+    static int ans;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        dist = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                dist[i][j] = INF;
+            }
+        }
+
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+
+            dist[from][to] = 1;
+        }
+    }
+
+    private static void solve() {
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        ans = 0;
+        for (int i = 1; i <= N; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= N; j++) {
+                if (dist[i][j] != INF || dist[j][i] != INF) {
+                    cnt++;
+                }
+            }
+
+            if (cnt == N - 1) {
+                ans++;
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+}
