b42d9c604f5e3ba1e7de35e31ffe0f98d0e66198
SangHyeon EUN <91714677+COBI-98@users.noreply.github.com>
2024-02-13 23:25:47 +0900

    [Gold V] Title: 괄호의 값, Time: 128 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+    static Deque<Character> dq = new ArrayDeque<>(30);
+    static Boolean flag = false;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+        char[] line = br.readLine().toCharArray();
+        int ans = 0;
+        int temp = 1;
+        for (int i = 0; i < line.length; i++) {
+            char c = line[i];
+            if (c == '(') {
+                temp *= 2;
+                dq.addLast(c);
+            } else if (c == '[') {
+                temp *= 3;
+                dq.addLast(c);
+            } else if (!dq.isEmpty() && c == ']' && dq.peekLast() == '[') {
+                dq.pollLast();
+                if (line[i - 1] == '[') {
+                    ans += temp;
+                }
+                temp /= 3;
+            } else if (!dq.isEmpty() && c == ')' && dq.peekLast() == '(') {
+                dq.pollLast();
+                if (line[i - 1] == '(') {
+                    ans += temp;
+                }
+                temp /= 2;
+            } else {
+                flag = true;
+                break;
+            }
+        }
+        if (!dq.isEmpty()) {
+            flag = true;
+        }
+        sb.append(flag ? 0 : ans);
+        System.out.print(sb);
+    }
+}
