0d6f636860603df5ab5492baf55b0869b56f7eac
SangHyeon EUN <91714677+COBI-98@users.noreply.github.com>
2024-08-11 23:57:00 +0900

    [Gold V] Title: 디저트, Time: 572 ms, Memory: 98660 KB -BaekjoonHub

---
+++ 백준/Gold/17953. 디저트/디저트.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[][] foods;
+    static int[][] dp = new int[100010][11];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        foods = new int[M + 1][N];
+
+        for (int i = 1; i < M + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                foods[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int[] d : dp) {
+            Arrays.fill(d, -1);
+        }
+
+        int ans = recur(0, 0);
+
+        System.out.println(ans);
+    }
+
+    static int recur(int cur, int prv) {
+        if (cur == N) {
+            return 0;
+        }
+
+        if (dp[cur][prv] != -1) {
+            return dp[cur][prv];
+        }
+
+        int ret = 0, taste;
+        for (int i = 1; i <= M; i++) {
+            if (prv == i) {
+                taste = foods[i][cur] / 2;
+            } else {
+                taste = foods[i][cur];
+            }
+
+            ret = Math.max(ret, recur(cur + 1, i) + taste);
+        }
+
+        dp[cur][prv] = ret;
+        return ret;
+    }
+}
